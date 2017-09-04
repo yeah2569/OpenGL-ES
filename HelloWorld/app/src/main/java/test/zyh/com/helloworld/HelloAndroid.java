f0521de57ea963e114108cbc0a80bbce9457b76b
yeah2569 <yeah2569@yeah.net>
2017-09-04 14:09:03 +0800

    创建Android项目

---
+++ HelloWorld/app/src/main/java/test/zyh/com/helloworld/HelloAndroid.java
@@ -0,0 +1,13 @@
+package test.zyh.com.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
