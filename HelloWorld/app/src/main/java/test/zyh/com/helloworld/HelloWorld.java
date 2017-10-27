da740f852453e5ff0268ebb0b69d978bf9e086cc
yeah2569 <yeah2569@yeah.net>
2017-10-27 15:37:43 +0800

    测试隐藏虚拟按键

--- HelloWorld/app/src/main/java/test/zyh/com/helloworld/HelloWorld.java
+++ HelloWorld/app/src/main/java/test/zyh/com/helloworld/HelloWorld.java
@@ -2,13 +2,22 @@ package test.zyh.com.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HelloWorld extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
 
+        setContentView(R.layout.main);
+
+        // 以下是Android 4.4以后自动隐藏虚拟键全屏显示的组合代码
+        View view = getWindow().getDecorView();
+        view.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
     }
 }
